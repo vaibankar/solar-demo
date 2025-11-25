ab607c1ab03fa9e8ab5d0c8f9103566d2d1bff20
vaibankar <113019542+vaibankar@users.noreply.github.com>
2025-11-25 11:08:49 +0530

    Delete backend directory

--- backend/src/config.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-module.exports = {
-  PORT: process.env.PORT || 4000,
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://mongo-svc:27017/solarapp',
-  JWT_SECRET: process.env.JWT_SECRET || 'verysecret',
-};
